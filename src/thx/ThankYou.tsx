b3d42d1448fcf45379c503c82886a74b3de162d5
Igor Kirpinev <kirpinevigor@yandex.ru>
2025-01-30 14:02:18 +0300

    fix thank you page

--- src/thx/ThankYou.tsx
+++ src/thx/ThankYou.tsx
@@ -7,7 +7,7 @@ import { Gap } from "@alfalab/core-components/gap";
 
 export const ThankYou = () => (
   <>
-    <div className={thxSt.container} style={{ height: "100%" }}>
+    <div className={thxSt.container} style={{ height: "100vh" }}>
       <img alt="Картинка смайлика" src={cool} width={200} />
       <Gap size={48} />
       <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
